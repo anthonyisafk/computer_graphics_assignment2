6f4aa8fb7b80e28f8bbfdff8477cf2631d8f2e68
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-05-22 13:03:28 +0300

    Add get_full_image_dimensions() and crop_image() to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2 as cv
 
 def get_ccs_unit_vectors(c_org: np.ndarray, c_lookat: np.ndarray, c_up: np.ndarray):
 	"""Calculates the camera unit vectors depending on where the camera is located,
@@ -17,12 +18,30 @@ def get_ccs_unit_vectors(c_org: np.ndarray, c_lookat: np.ndarray, c_up: np.ndarr
 	return x_c, y_c, z_c
 
 
-def find_out_of_bounds_vertices(verts_rast, img_h, img_w):
-	negative = np.argwhere(verts_rast < 0)[:, 0]
-	height = np.argwhere(verts_rast[:, 0] > img_w - 1)[:, 0]
-	width = np.argwhere(verts_rast[:, 1] > img_h - 1)[:, 0]
-	return np.union1d(np.union1d(height, width), negative)
+def get_full_image_dimensions(img_w, img_h, max_w, max_h, M, N):
+	crop = np.array([0, 0])
+	full_img_w = max(img_w, max_w + 1)
+	full_img_h = max(img_h, max_h + 1)
+	if M > img_w / 2:
+		crop[0] = M - img_w / 2
+		if full_img_w == img_w:
+			full_img_w = M + img_w / 2
+	if N > img_h / 2:
+		crop[1] = N - img_h / 2
+		if full_img_h == img_h:
+			full_img_h = N + img_h / 2
+
+	return crop, full_img_w, full_img_h
 
 
-def get_in_bounds_vertices_and_faces(rejects, verts_rast, faces, vcolors):
-	verts_in = np.delete(verts_rast, rejects, 0)
+def crop_image(img, crop, img_w, img_h):
+	cropped_img = np.zeros((img_w, img_h, 3))
+	for i in range(img_w):
+		x = i + crop[0]
+		for j in range(img_h):
+			cropped_img[i][j] = img[x][j + crop[1]]
+	cv.imshow(f"Cropped image", cropped_img)
+	cv.waitKey(0)
+	cv.destroyAllWindows()
+
+	return cropped_img
