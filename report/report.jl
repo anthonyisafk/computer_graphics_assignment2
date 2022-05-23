97a3ee7fd27389d55d44326614adca5cc285f974
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-05-24 02:57:44 +0300

    Start working on the report

---
+++ report/report.jl
@@ -0,0 +1,293 @@
+### A Pluto.jl notebook ###
+# v0.17.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8cc5ec30-dae5-11ec-1e85-153f732e3d57
+begin
+	using Markdown
+	using InteractiveUtils
+	using PlutoUI
+end
+
+# ╔═╡ 805c46e7-218c-4fc6-9dd8-8b1b38b6ea6c
+md"""
+# Γραφική με Υπολογιστές
+## Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης - Τμήμα Ηλεκτρολόγων Μηχανικών Μηχανικών Υπολογιστών
+### 1η Εργασία Εξαμήνου: Μετασχηματισμοί και Προβολές 
+### Αντωνίου Αντώνιος - aantonii@ece.auth.gr - 9482 
+"""
+
+# ╔═╡ 3e4644be-6cac-41ab-b8ac-75dca0984e37
+md"""
+## Σκοπός της εργασίας
+Στο δεύτερο σκέλος των εργασιών του μαθήματος, αναλαμβάνουμε επί της ουσίας να δημιουργήσουμε τα arguments που μας είχαν δοθεί για την υλοποίηση της συνάρτησης `render()` του πρώτου παραδοτέου. Αυτό σημαίνει πως μας δίνονται ως δεδομένα:
+* Οι συντεταγμένες στον τρισδιάστατο κόσμο **(θα το συναντήσουμε πολλές φορές ως WCS - World Coordinate System)** των κορυφών των τριγώνων που απαρτίζουν ένα αντικείμενο $(verts_{3D})$,
+* Οι συντεταγμένες της κάμερας **$(c_{org})$**,
+* Οι συντεταγμένες του σημείου που θεωρείται *στόχος της κάμερας*, δηλαδή το κέντρο της φωτογραφίας που θα παραχθεί **$(c_{lookat})$**,
+* Το μοναδιαίο διάνυσμα **up** **$(c_{up})$**: ποια διεύθυνση θεωρείται κατακόρυφη, και
+* Την εστιακή απόσταση f.
+Ωστόσο, για να φτάσουμε στο σημείο να καλέσουμε τη `render_object()`, που υπολογίζει τις προβολές των σημείων και καλεί τη `render()`, πρέπει πρώτα να υλοποιήσουμε μία σειρά από συναρτήσεις, οι οποίες αναλύονται παρακάτω.
+
+## $affine\_transform(c_{p},\theta,u,t)$
+Της δίνεται ένα σημείο (ή πίνακας από σημεία) cp και επιστρέφει τις συντεταγμένες του σημείου/των σημείων μετά από έναν Affine μετασχηματισμό, ο οποίος αποτελείται από μία περιστροφή γύρω από άξονα $u$ ή μια μετατοπίση κατά $t$ ή έναν συνδυασμό αυτών.
+Για την υλοποίηση της περιστροφής, σχηματίζουμε τον **πίνακα R**, βασιζόμενοι στον _**τύπο του Rodrigues**_:
+
+$(1-cos\theta)\cdot\begin{bmatrix}u^{2}_{x} & u_{x}u_{y} & u_{x}u_{y}\\ u_{y}u_{x} & u^{2}_{y} & u_{y}u_{z}\\ u_{z}u_{x} & u_{z}u_{y} & u^{2}_{z}\end{bmatrix}+cos\theta\cdot\begin{bmatrix}1 & 0 & 0\\ 0 & 1 & 0\\ 0 & 0 & 1\end{bmatrix}+sin\theta\cdot\begin{bmatrix}0 & -u_{z} & u_{y}\\ u_{z} & 0 & -u_{x}\\ -u_{y} & u_{x} & 0\end{bmatrix}$
+
+Ύστερα, αν ορίζεται από τα ορίσματα, πραγματοποείται και μία μετατόπιση κατά t, που υλοποιείται με απλή πρόσθεση των συντεταγμένων του διανύσματος στις συντεταγμένες του διανύσματος θέσης κάθε σημείου.
+
+## $system\_transform(c_{p}, R, c_{o})$
+_R_ είναι ο πίνακας περιστροφής που εξάγεται για την αλλαγή των συντεταγμένων ενός σημείου σε ένα νέο σύστημα συντεταγμένων, με νέα αρχή το $c_{o}$.
+Σύμφωνα με τη θεωρία επιστρέφουμε το διάνυσμα:
+
+$R^{T}\cdot c_{p}-c_{o}$
+
+## $project\_cam(f,c_{v},c_{x},c_{y},c_{z},p)$
+Επιστρέφει τις προβολές των σημείων που βρίσκονται στον πίνακα _p_, σε μια φωτογραφία στον "κόσμο" **(CCS - Camera Coordinate System)** της οποίας έχουμε βάση $c_{x},c_{y},c_{z}$ και αρχή $c_{v}$, με εστιακή απόσταση _f_.
+Για την επίτευξη αυτού του σκοπού καλούμε τη `system_transform()`, παράγουμε τις καινούριες συντεταγμένες (έστω $x,y,z$) στο CCS και έπειτα, ξανά από τη θεωρία:
+
+* $x'=f\cdot\frac{x}{z}$
+* $y'=f\cdot\frac{y}{z}$
+* $depth=z$
+
+Τα βάθη _depth_ είναι πληροφορία που χρειάζεται η `render()` για να καθορίσει με ποια σειρά θα χρωματιστούν τα τρίγωνα με τα οποία τροφοδοτείται.
+
+## $project\_cam\_lookat(f,c_{org},c_{lookat},c_{up},verts_{3d})$
+Της δίνονται όλες οι κορυφές $verts_{3d}$ των τριγώνων, που δίνονται μία-μία στην `project_cam()` μαζί με τη **βάση του CCS**, αφού έχει εξαχθεί βάσει της θεωρίας:
+* $\overline{CA} = c_{lookat}-c_{org}$
+* $\hat{z_{c}}=\frac{\overline{CA}}{||CA||}$
+* $\overline{t}=c_{up}-<c_{up},\hat{z_{c}}>\cdot\hat{z_{c}}$
+* $\hat{y_{c}}=\frac{\overline{t}}{||t||}$
+* $\hat{x_{c}}=\hat{y_{c}}\times\hat{z_{c}}$
+
+Επιστρέφει τα ίδια δεδομένα με τη συνάρτηση που καλεί.
+
+## $rasterize(verts_{2d}, img_{h}, img_{w}, cam_{h}, cam_{w})$
+Αφού οι κορυφές προβληθούν σε ένα επίπεδο 2 διαστάσεων, πρέπει να αντιστοιχίσουμε τιε θέσεις τους με τα indices της εικόνας `img`, η οποία είναι ένας πίνακας $img_{w}\times img_{h}\times 3$, δηλαδή ένας χάρτης με τις RGB τιμές κάθε pixel της φωτογραφίας.
+Για να γίνει αυτή η αντιστοίχιση, επιλέγουμε να μεγεθύνουμε τον πίνακα αυτό αρκετά, ώστε να χωράει όλο το αντικείμενο που φωτογραφίσαμε, ασχέτως αν θα βρίσκεται όλο στην τελική φωτογραφία ή όχι. Οπότε κάνουμε μετατόπιση τέτοια, ώστε όλα τα indices που θα επιστρέψει η `rasterize()` να είναι θετικά (η ελάχιστη τιμή της μετατόπισης είναι $\frac{img_w}{2}$ κατά x και $\frac{img_h}{2}$ κατά y).
+H συνάρτηση επιστρέφει την ποσότητα κατά την οποία έκανε offset όλους τους δείκτες, καθώς τα δεδομένα αυτά χρειάζονται στο τελευταίο βήμα του αλγορίθμου.
+
+## Χρησιμοποιώντας τα παραπάνω στη render_object()
+
+"""
+
+# ╔═╡ 44338cb5-b26c-455b-aae3-d8c4e42542c0
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+InteractiveUtils = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+Markdown = "d6f4376e-aef5-505a-96c1-9c027394607a"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoUI = "~0.7.39"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+[[AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "8eaf9f1b4921132a4cff3f36a1d9ba923b14a481"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.1.4"
+
+[[ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+
+[[Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "a985dc37e357a3b22b260a5def99f3530fb415d3"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.2"
+
+[[Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[Downloads]]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+
+[[FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.4"
+
+[[Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "8d511d5b81240fc8e6802386302675bdf47737b9"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.4"
+
+[[HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "c47c5fa4c5308f27ccaac35504858d8914e102f9"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.4"
+
+[[IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "f7be53659ab06ddc986428d3a9dcc95f6fa6705a"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.2"
+
+[[InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
+
+[[LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+
+[[LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+
+[[LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+
+[[Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[LinearAlgebra]]
+deps = ["Libdl"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+
+[[Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+
+[[NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "1285416549ccfcdf0c50d4997a94331e88d68413"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.3.1"
+
+[[Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+
+[[PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
+git-tree-sha1 = "8d1f54886b9037091edf146b517989fc4a09efec"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.39"
+
+[[Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[Random]]
+deps = ["Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+
+[[Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+[[Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+
+[[Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[Tricks]]
+git-tree-sha1 = "6bac775f2d42a611cdfcd1fb217ee719630c4175"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.6"
+
+[[UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+
+[[nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+
+[[p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+"""
+
+# ╔═╡ Cell order:
+# ╠═8cc5ec30-dae5-11ec-1e85-153f732e3d57
+# ╠═805c46e7-218c-4fc6-9dd8-8b1b38b6ea6c
+# ╠═3e4644be-6cac-41ab-b8ac-75dca0984e37
+# ╠═44338cb5-b26c-455b-aae3-d8c4e42542c0
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
